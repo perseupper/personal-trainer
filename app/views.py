ecf85f9bbee9f94785f0aecf3c2f30ebbed5d465
David Gwilt <dave@Davids-MacBook-Pro.local>
2016-01-23 17:39:44 +0000

    first template

--- app/views.py
+++ app/views.py
@@ -3,4 +3,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'}  # fake user
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
