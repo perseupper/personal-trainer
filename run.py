be03011a7aa7c6d97ae0060cc0fde9984c2c0ad3
David Gwilt <dave@Davids-MacBook-Pro.local>
2016-01-23 18:30:49 +0000

    shows ip address

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 from app import app
 from sense_hat import SenseHat
+import netifaces as ni
+
+ni.ifaddresses('eth0')
+ip = ni.ifaddresses('eth0')[2][0]['addr']
 
 sense = SenseHat()
 
-sense.show_message("Starting...")
+sense.show_message("Starting on {}".format(ip))
 app.run(host='0.0.0.0', port=80, debug=True)
